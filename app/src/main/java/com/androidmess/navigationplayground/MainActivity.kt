2df3df95f1d72e93efe98f579134476c99745a74
Sebastian Swierczek <sebastian.swierczek@gmail.com>
2018-07-19 10:13:30 +0200

    Add Toolbar

--- app/src/main/java/com/androidmess/navigationplayground/MainActivity.kt
+++ app/src/main/java/com/androidmess/navigationplayground/MainActivity.kt
@@ -4,18 +4,25 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
-import kotlinx.android.synthetic.main.main_activity.bottomNavigation
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        setupToolbar()
         bottomNavigation.setupWithNavController(navController())
     }
 
     override fun onSupportNavigateUp() =
-        navController().navigateUp()
+            navController().navigateUp()
+
+    private fun setupToolbar() {
+        mainToolbar.title = getString(R.string.menu_home_text)
+        setSupportActionBar(mainToolbar)
+        mainToolbar.setupWithNavController(navController())
+    }
 
     private fun AppCompatActivity.navController() = findNavController(this, R.id.nav_host_fragment)
 }
