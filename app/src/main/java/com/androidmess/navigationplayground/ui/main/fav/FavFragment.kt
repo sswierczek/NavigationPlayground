4b519755e11f767f8337025ebf88b0e0ff0e041f
Sebastian Swierczek <sebastian.swierczek@gmail.com>
2018-07-19 11:45:05 +0200

    Add deeplink support

--- app/src/main/java/com/androidmess/navigationplayground/ui/main/fav/FavFragment.kt
+++ app/src/main/java/com/androidmess/navigationplayground/ui/main/fav/FavFragment.kt
@@ -6,8 +6,9 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.androidmess.navigationplayground.R
+import kotlinx.android.synthetic.main.fav_fragment.*
+
 
 class FavFragment : Fragment() {
 
@@ -18,10 +19,15 @@ class FavFragment : Fragment() {
     private lateinit var viewModel: FavViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fav_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        textId.text = arguments?.getString("id")
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
